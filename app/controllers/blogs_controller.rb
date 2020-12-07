9d3952af7824c65f4de685862abf2fc735c9e079
Taiga-Soma <allvictory111@gmail.com>
2020-12-07 19:24:22 +0900

    ログイン画面実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @blogs = Blog.all
   end
@@ -29,5 +31,11 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.require(:blog).permit(:title, :sentence, :date,images: [])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
